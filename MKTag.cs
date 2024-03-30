93980542e337707889583b3cc5b71b65e3d1d5b5
legender2 <43158716+tyster220@users.noreply.github.com>
2024-03-30 01:38:48 -0400

    updated submodule

--- MKTag.cs
+++ MKTag.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Minikit.Abilities
+namespace Minikit.AbilitySystem
 {
     public class MKTag : IEquatable<MKTag>
     {
